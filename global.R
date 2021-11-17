1f1bf21e5a490048add05bc4c7233311ba2139b4
Aaron Clark <aaron.clark@biogen.com>
2021-11-17 11:54:52 -0500

    tailor global.R file for library()-ing each package individually to help with shinyapps.io deployment.

--- global.R
+++ global.R
@@ -1,49 +1,51 @@
+
 # Packages needed for the app.
-packages = c("shiny"
-             ,"shinyhelper"
-             ,"shinyjs"
-             ,"shinydashboard"
-             ,"shinyWidgets"
-             ,"data.table"
-             ,"DT"
-             ,"readr"
-             ,"lubridate"
-             ,"RSQLite"
-             ,"DBI"
-             ,"rvest"
-             ,"xml2"
-             ,"httr"
-             ,"desc"
-             ,"dplyr"
-             ,"tools"
-             ,"stringr"
-             ,"tidyverse"
-             ,"loggit"
-             ,"shinycssloaders"
-             ,"rAmCharts"
-             ,"devtools"
-             ,"plotly"
-             ,"cranlogs"
-             ,"formattable"
-             ,"rintrojs"
-)
-
-# Install and load required packages.
-package.check <- lapply(
-  packages,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-)
-
-# Install riskmetric from github and load it
-if(!require(riskmetric)){
-  devtools::install_github("pharmaR/riskmetric")
-  library(riskmetric)
-}
+# Doesn't work when deploying to shinyapps.io
+# packages = c("shiny"
+#              ,"shinyhelper"
+#              ,"shinyjs"
+#              ,"shinydashboard"
+#              ,"shinyWidgets"
+#              ,"data.table"
+#              ,"DT"
+#              ,"readr"
+#              ,"lubridate"
+#              ,"RSQLite"
+#              ,"DBI"
+#              ,"rvest"
+#              ,"xml2"
+#              ,"httr"
+#              ,"desc"
+#              ,"dplyr"
+#              ,"tools"
+#              ,"stringr"
+#              ,"tidyverse"
+#              ,"loggit"
+#              ,"shinycssloaders"
+#              ,"rAmCharts"
+#              ,"devtools"
+#              ,"plotly"
+#              ,"cranlogs"
+#              ,"formattable"
+#              ,"rintrojs"
+# )
+# 
+# # Install and load required packages.
+# package.check <- lapply(
+#   packages,
+#   FUN = function(x) {
+#     if (!require(x, character.only = TRUE)) {
+#       install.packages(x, dependencies = TRUE)
+#       library(x, character.only = TRUE)
+#     }
+#   }
+# )
+# 
+# # Install riskmetric from github and load it
+# if(!require(riskmetric)){
+#   devtools::install_github("pharmaR/riskmetric")
+#   library(riskmetric)
+# }
 
 # Load the functions to create infoboxes.
 source("Utils/infoboxes.R")
@@ -71,30 +73,30 @@ sidebar_steps <-
 # Instead comment the code that install packages and attach them directly by
 # uncommenting the lines below.
 
-# library(shiny)
-# library(shinyhelper)
-# library(shinyjs)
-# library(shinydashboard)
-# library(shinyWidgets)
-# library(data.table)
-# library(DT)
-# library(readr)
-# library(lubridate)
-# library(RSQLite)
-# library(DBI)
-# library(rvest)
-# library(xml2)
-# library(httr)
-# library(desc)
-# library(dplyr)
-# library(tools)
-# library(stringr)
-# library(tidyverse)
-# library(loggit)
-# library(shinycssloaders)
-# library(rAmCharts)
-# library(devtools)
-# library(plotly)
-# library(riskmetric) # devtools::install_github("pharmaR/riskmetric")
-# library(formattable)
-# library(rintrojs)
+library(shiny)
+library(shinyhelper)
+library(shinyjs)
+library(shinydashboard)
+library(shinyWidgets)
+library(data.table)
+library(DT)
+library(readr)
+library(lubridate)
+library(RSQLite)
+library(DBI)
+library(rvest)
+library(xml2)
+library(httr)
+library(desc)
+library(dplyr)
+library(tools)
+library(stringr)
+library(tidyverse)
+library(loggit)
+library(shinycssloaders)
+library(rAmCharts)
+library(devtools)
+library(plotly)
+library(riskmetric) # devtools::install_github("pharmaR/riskmetric")
+library(formattable)
+library(rintrojs)
